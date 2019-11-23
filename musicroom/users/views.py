1dd56f553ea3fbc71b817a128fad32d36e4a0809
anchern <anton.a.chernysh@gmail.com>
2019-11-23 19:21:10 +0200

    add me/ endpoint

--- musicroom/users/views.py
+++ musicroom/users/views.py
@@ -1,3 +1,24 @@
+import json
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import UserProfile
+from .serializers import UserProfileSerializer
+from rest_framework import views
+
+# def profile(request):
+#     User = get_user_model()
+#     user = User.objects.all()
+#     user_json = json.dumps(user)
+#     return HttpResponse(user_json)
+
+
+class UserProfileView(views.APIView):
+    def get(self, request, format=None):
+        user_profile = UserProfile.objects.get(pk=request.user.id)
+        serializer = UserProfileSerializer(user_profile)
+        return Response(serializer.data)
